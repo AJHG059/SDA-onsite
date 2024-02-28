5248291f9305cd415c7933bc160a51281bce0980
AJHG059 <135741598+AJHG059@users.noreply.github.com>
2024-02-28 09:15:52 +0300

    first commit

---
+++ src/test/java/day06/C01JavaFakerClass.java
@@ -0,0 +1,34 @@
+package day06;
+
+import com.github.javafaker.Faker;
+import org.junit.Test;
+
+public class C01JavaFakerClass {
+
+
+    @Test
+    public void testFaker(){
+
+        //this cruting random/Fake name/any string
+    Faker faker  = new Faker();
+    String fullname1 = faker.name().fullName();
+    String fullname2 = faker.name().fullName();
+
+        System.out.println("fullname1 = " + fullname1);
+        System.out.println("fullname2 = " + fullname2);
+
+
+        //random number you can use limt number by use number between
+        System.out.println("faker.number() = " + faker.number().numberBetween(100,999));
+
+        //you can print to value or mort but you mast save it in object to store
+        System.out.println(fullname1 + " + " + fullname2);
+
+        // cruting fake address and full address
+        System.out.println("faker.address().fullAddress() = " + faker.address().fullAddress());
+
+        //we can cruting fake intrint like domin name or password in multible condiction you can add any term you want
+        System.out.println(faker.internet().password(6, 8, true, true, false));
+    }
+
+}
