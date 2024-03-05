5093a609c1bf5590ba5491fd6d1e886e753a0677
AJHG059 <135741598+AJHG059@users.noreply.github.com>
2024-03-05 17:21:50 +0300

    aa

---
+++ src/test/java/chaling/day10.java
@@ -0,0 +1,30 @@
+package chaling;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.JScriptUtilities;
+import utilities.TestBase;
+
+public class day10 extends TestBase {
+
+
+    @Test
+    public void test(){
+
+        driver.get("https://www.youtube.com");
+    WebElement button = driver.findElement(By.xpath("//input[@id='search']"));
+        JScriptUtilities.clickElementByJS(driver,button);
+    button.sendKeys("Hans Zimmer - Mountains (Interstellar Soundtrack)");
+   WebElement button1 = driver.findElement(By.xpath("//button[@id='search-icon-legacy']"));
+    button1.click();
+    WebElement vi = driver.findElement(By.xpath("//*[contains(text() , 'Hans Zimmer - Mountains (Interstellar Soundtrack)')]"));
+//    vi.click();
+    JScriptUtilities.clickElementByJS(driver,vi);
+        Assert.assertTrue(vi.isEnabled());
+    }
+
+
+
+}
--- src/test/java/day10/C02RebotClass.java
+++ src/test/java/day10/C02RebotClass.java
@@ -1,5 +1,6 @@
 package day10;
 
+import org.checkerframework.checker.units.qual.Speed;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -11,7 +12,6 @@ import java.awt.event.KeyEvent;
 public class C02RebotClass extends TestBase {
 
 
-
     @Test
     public void test() throws AWTException, InterruptedException {
 
@@ -20,16 +20,16 @@ public class C02RebotClass extends TestBase {
         //cruate new robort for the mous
         Robot robot = new Robot();
 
-   WebElement searchBox = driver.findElement(By.id("gh-ac"));
-   searchBox.click();
-   robot.keyPress(KeyEvent.VK_SHIFT);
-   robot.keyPress(KeyEvent.VK_A);
-   robot.keyRelease(KeyEvent.VK_A);
-   robot.keyRelease(KeyEvent.VK_SHIFT);
-   Thread.sleep(2000);
-   robot.keyPress(KeyEvent.VK_ENTER);
-   robot.keyRelease(KeyEvent.VK_ENTER);
+        WebElement searchBox = driver.findElement(By.id("gh-ac"));
+        searchBox.click();
+        robot.keyPress(KeyEvent.VK_SHIFT);
+        robot.keyPress(KeyEvent.VK_A);
+        robot.keyRelease(KeyEvent.VK_A);
+        robot.keyRelease(KeyEvent.VK_SHIFT);
+        Thread.sleep(2000);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
 
-    robot.mouseWheel(30);
+        robot.mouseWheel(30);
     }
 }
